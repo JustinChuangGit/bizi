462a473da7bc6a1d3fa3b6cd8119c5ebc2e9afc2
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2023-10-07 20:24:38 -0400

    XCode 15 error fixed.

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'plusJakartaSans'),
       home: const signInScreen(),
     );
---
+++ lib/screens/qrScanScreen/qrScanScreen.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/material.dart';
+// import 'package:mobile_scanner/mobile_scanner.dart';
+
+// class qrScanScreen extends StatelessWidget {
+//   const qrScanScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: 
+//     );
+//   }
+// }
