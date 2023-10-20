13d4ff58e66a4faf7d611be141ec2081d163f3eb
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2023-10-19 23:45:40 -0400

    More fields added

--- lib/utilities/models/rewardModel.dart
+++ lib/utilities/models/rewardModel.dart
@@ -7,18 +7,23 @@ class RewardModel {
   final String offerFilePath;
   final int offerUses = 0;
   String? savings;
+  String? dateCreated;
+  bool activeState;
 
   //class with offerings
   //class redeem products
 
-  RewardModel(
-      {required this.id,
-      required this.vendorName,
-      required this.normalPrice,
-      required this.newPrice,
-      required this.offerName,
-      required this.offerFilePath,
-      this.savings});
+  RewardModel({
+    required this.id,
+    required this.vendorName,
+    required this.normalPrice,
+    required this.newPrice,
+    required this.offerName,
+    required this.offerFilePath,
+    this.activeState = true,
+    this.dateCreated,
+    this.savings,
+  });
 
   toJson() {
     return {
@@ -31,15 +36,20 @@ class RewardModel {
       'offerUses': offerUses,
       'savings': (double.tryParse(normalPrice)! - double.tryParse(newPrice)!)
           .toString(),
+      'dateCreated': DateTime.now().toString(),
+      'activeState': activeState
     };
   }
 
   factory RewardModel.fromJson(Map<String, dynamic> json) => RewardModel(
-      id: json['id'],
-      vendorName: json['vendorName'],
-      normalPrice: json['normalPrice'],
-      newPrice: json['newPrice'],
-      offerName: json['offerName'],
-      offerFilePath: json['offerFilePath'],
-      savings: json['savings']);
+        id: json['id'],
+        vendorName: json['vendorName'],
+        normalPrice: json['normalPrice'],
+        newPrice: json['newPrice'],
+        offerName: json['offerName'],
+        offerFilePath: json['offerFilePath'],
+        savings: json['savings'],
+        dateCreated: json['dateCreated'],
+        activeState: json['activeState'],
+      );
 }
